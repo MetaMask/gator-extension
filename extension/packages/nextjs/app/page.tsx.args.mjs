e3587949fb4edc2067ef4cd05891c547bedcbfa4
AyushBherwani1998 <ayush.bherwani1998@gmail.com>
2025-05-26 13:52:17 +0530

    update page template

---
+++ extension/packages/nextjs/app/page.tsx.args.mjs
@@ -0,0 +1,15 @@
+export const description = `
+<p className="text-center text-lg">
+  Please update the{" "}
+  <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
+    .env
+  </code>{" "}
+  file with the{" "}
+  <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
+    PIMLICO_API_KEY
+  </code>{" "}
+  variable.
+</p>
+`;
+
+export const externalExtensionName = "Delegation Toolkit";
