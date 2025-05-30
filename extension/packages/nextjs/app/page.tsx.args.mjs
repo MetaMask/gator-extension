344268e08587f13f8534e8fe3ad3f06df2530d45
AyushBherwani1998 <ayush.bherwani1998@gmail.com>
2025-05-30 09:17:33 +0530

    improve configuration message

--- extension/packages/nextjs/app/page.tsx.args.mjs
+++ extension/packages/nextjs/app/page.tsx.args.mjs
@@ -1,22 +1,29 @@
 export const description = `
-<p className="text-center text-lg">
-  Please update the{" "}
-  <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-    .env
-  </code>{" "}
-  file with the{" "}
-  <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-    PIMLICO_API_KEY
-  </code>{" "}
-  variable.
-</p>
-<p className="text-center text-lg">
-  Please check the{" "}
-  <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-    .env.example
-  </code>{" "}
-  for reference.
-</p>
+<div className="flex justify-center mt-8">
+  <div className="flex flex-col bg-base-100 px-6 py-4 text-center items-center max-w-4xl rounded-xl border border-base-300 space-y-4">
+    <h2 className="text-lg font-semibold text-yellow-500">Configuration required</h2>
+    
+    <p className="text-center text-lg">
+      Please update the{" "}
+      <code className="italic bg-base-200 text-base font-bold max-w-full break-words break-all inline-block">
+        .env
+      </code>{" "}
+      file with the{" "}
+      <code className="italic bg-base-200 text-base font-bold max-w-full break-words break-all inline-block">
+        PIMLICO_API_KEY
+      </code>{" "}
+      variable.
+    </p>
+    
+    <p className="text-center text-lg">
+      Please check the{" "}
+      <code className="italic bg-base-200 text-base font-bold max-w-full break-words break-all inline-block">
+        .env.example
+      </code>{" "}
+      for reference.
+    </p>
+  </div>
+</div>
 `;
 
 export const externalExtensionName = "Delegation Toolkit";
